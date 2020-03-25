c7381073e9aec78b6b1a1827a97212d53958341b
nininv <nininv45@gmail.com>
2020-03-25 16:45:39 +0530

    Url Update

--- src/store/http/competitionManagementHttp/competitionManagementHttp.js
+++ src/store/http/competitionManagementHttp/competitionManagementHttp.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const competitionManagementHttp = axios.create({
 
-    baseURL: "https://competition-api-dev.worldsportaction.com",
+    baseURL: "http://a90830342b690491db1e89b76ed1d900-512956195.ap-southeast-2.elb.amazonaws.com/competition",
 });
 
 competitionManagementHttp.interceptors.request.use(function (config) {
--- src/store/http/liveScoreHttp/liveScorehttp.js
+++ src/store/http/liveScoreHttp/liveScorehttp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
     // baseURL: "http://13.237.1.133:8085"
     // baseURL: "https://world-sport-action-socket.appspot.com"
-    baseURL: "https://livescores-api-dev.worldsportaction.com/"
+    baseURL: "http://a90830342b690491db1e89b76ed1d900-512956195.ap-southeast-2.elb.amazonaws.com/livescores/"
     // baseURL: "http://192.168.3.98:3001"
     // baseURL: "http://192.168.3.98:8083/"
 });
