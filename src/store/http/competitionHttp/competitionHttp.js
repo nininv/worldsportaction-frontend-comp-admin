906cee60ef078ed2750e1dc5a13ea53b44bc1a9a
nininv <nininv45@gmail.com>
2020-03-30 15:26:40 +0530

    Empty Commit

--- src/store/http/competitionHttp/competitionHttp.js
+++ src/store/http/competitionHttp/competitionHttp.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const competitionHttp = axios.create({
-
-    // baseURL: "http://192.168.3.98:3005",
     baseURL: "https://netball-api-stg.worldsportaction.com/competition"
 });
 
