7296535d5ad2c92acae149a923aa25d396ed3e6a
nininv <nininv45@gmail.com>
2020-04-21 23:15:30 +0530

    Team list change

--- src/store/http/liveScoreHttp/liveScorehttp.js
+++ src/store/http/liveScoreHttp/liveScorehttp.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const http = axios.create({
     // baseURL: "http://13.237.1.133:8085"
     // baseURL: "https://world-sport-action-socket.appspot.com"
-    baseURL: "https://livescores-api-dev.worldsportaction.com/"
     // baseURL: "http://192.168.3.98:3001"
     // baseURL: "http://192.168.31.99:8083/"
+    baseURL: "https://livescores-api-dev.worldsportaction.com/"
 });
 
 http.interceptors.request.use(function (config) {
