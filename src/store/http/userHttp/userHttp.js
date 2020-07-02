dd91b64fc7a0964d17510a05bd14715ec7a2fa5a
nininv <nininv45@gmail.com>
2020-07-02 15:35:06 +0200

    Update endpoints with .env

--- src/store/http/userHttp/userHttp.js
+++ src/store/http/userHttp/userHttp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_USER_API_URL,
 });
 
 http.interceptors.request.use(function (config) {
