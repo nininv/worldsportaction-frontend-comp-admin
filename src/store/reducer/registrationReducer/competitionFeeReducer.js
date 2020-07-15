6827e43e7943fd404fefe56dbaa1cd0e3b683ce0
nininv <nininv45@gmail.com>
2020-07-16 00:26:07 +0530

    CM-1075 - fix
    (cherry picked from commit 8af85af517f91bcca6c067e59b2fd21e4bae4e5b)

--- src/store/reducer/registrationReducer/competitionFeeReducer.js
+++ src/store/reducer/registrationReducer/competitionFeeReducer.js
@@ -137,7 +137,8 @@ const initialState = {
 
     affiliateArray: [],
     anyOrgAffiliateArr: [],
-    any_club_Org_AffiliateArr: []
+    any_club_Org_AffiliateArr: [],
+    createVenue: null
 };
 
 /////function to append isselected values in default membership types array
@@ -1442,6 +1443,7 @@ function competitionFees(state = initialState, action) {
             return { ...state, onLoad: true, error: null };
 
         case ApiConstants.API_REG_COMPETITION_LIST_SUCCESS:
+            console.log("API_REG_COMPETITION_LIST_SUCCESS",state.selectedVenuesAdd, state.selectedVenues )
             let competitionListData = action.result;
             state.selectedVenuesAdd = null
             state.selectedVenues = []
@@ -1600,6 +1602,15 @@ function competitionFees(state = initialState, action) {
             }
             state.postInvitees = allData.competitiondetail.invitees
             state.postVenues = allData.competitiondetail.venues
+
+            if (state.createVenue) {
+                let defaultPostVenueObject = {
+                    competitionVenueId: 0,
+                    venueId: state.createVenue.venueId
+                }
+                state.postVenues.push(defaultPostVenueObject)
+            }
+            
             state.charityTitle = isArrayNotEmpty(allData.competitionpayments.charityRoundUp) ? allData.competitionpayments.charityRoundUp[0].charityRoundUpName : ""
             state.charityDescription = isArrayNotEmpty(allData.competitionpayments.charityRoundUp) ? allData.competitionpayments.charityRoundUp[0].charityRoundUpDescription : ""
             state.onLoad = false
@@ -2160,6 +2171,7 @@ function competitionFees(state = initialState, action) {
         ///clearing particular reducer data
         case ApiConstants.REG_COMPETITION_FEES_CLEARING_REDUCER_DATA:
             if (action.dataName == "all") {
+                console.log("REG_COMPETITION_FEES_CLEARING_REDUCER_DATA", state.selectedVenuesAdd, state.selectedVenues)
                 const defaultDetailObj = {
                     competitionUniqueKey: "",
                     competitionName: "",
@@ -2264,13 +2276,10 @@ function competitionFees(state = initialState, action) {
                 updatedVenue['contactNumber'] = venueSuccess.contactNumber
                 state.venueList.push(updatedVenue)
                 state.selectedVenuesAdd = "Add"
+                console.log("state.selectedVenues", state.selectedVenues);
                 state.selectedVenues.push(venueSuccess.venueId)
-
-                let defaultPostVenueObject = {
-                    competitionVenueId: 0,
-                    venueId: venueSuccess.venueId
-                }
-                state.postVenues.push(defaultPostVenueObject)
+                state.createVenue = action.result
+               
             }
             return { ...state }
 
