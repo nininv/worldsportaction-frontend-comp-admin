922054025b644dafd06f1b9d42c814d5b6e1eec4
nininv <nininv45@gmail.com>
2020-08-04 11:12:16 +0300

    Fix place autocomplete

--- src/components/competition/elements/PlaceAutoComplete/index.js
+++ src/components/competition/elements/PlaceAutoComplete/index.js
@@ -121,6 +121,7 @@ const PlacesAutocomplete = ({
         onChange={handleInput}
         disabled={!ready}
         placeholder={AppConstants.pleaseInputAddress}
+        autoComplete="off"
         {...otherProps}
       />
       {status !== 'OK' && error && (
