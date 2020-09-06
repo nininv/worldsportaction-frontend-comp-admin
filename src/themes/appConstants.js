edee31aa4bfa7e143d23377e10193aca3f59bcac
nininv <nininv45@gmail.com>
2020-09-06 16:28:23 +0530

    crash issue
    (cherry picked from commit f370e9631acecc51160cfb6da43aa596ed8975fe)

--- src/themes/appConstants.js
+++ src/themes/appConstants.js
@@ -1575,7 +1575,8 @@ const AppConstants = {
   reasonWhyYourAreDecline: "Reason why you are declining the request for de-registration?",
   refundAmount: "Refund Amount",
   toDate: "To date",
-  fromDate: "From date"
+  fromDate: "From date",
+  noCompetitionYear:"There are no competitions for the selected year"
 };
 
 export default AppConstants;
