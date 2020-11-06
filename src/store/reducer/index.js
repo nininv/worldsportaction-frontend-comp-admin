9e8531376512ff24516ec470c25f09f4673cebeb
nininv <nininv45@gmail.com>
2020-11-06 20:56:59 +0000

    Squashed commit of the following
    commit 1e8cf099 
    Author: Oleksandr Didenko <goldbyol@outlook.com> 
    Date: Fri Nov 06 2020 23:10:16 GMT+1100 (Australian Eastern Daylight Time) 
    
        Merge branch 'develop' of https://git-codecommit.ap-southeast-2.amazonaws.com/v1/repos/wsa-comp-admin into bugfix/tfa
    
    
    commit 98b879ae 
    Author: Oleksandr Didenko <goldbyol@outlook.com> 
    Date: Fri Nov 06 2020 23:07:03 GMT+1100 (Australian Eastern Daylight Time) 
    
        Fix issue with TFA

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,12 +1,14 @@
-import { combineReducers } from "redux";
-import LoginState from "./authenticationReducer";
-import MenuNavigationState from "./menuNavigationReducer";
-import RegistrationState from "./registrationReducer/registrationReducer";
-import AppState from "./appReducer";
-////*****************Live Score**************************Start////
-import LiveScorePlayerState from "./liveScoreReducer/liveScorePlayerReducer";
-import LiveScoreMatchState from "./liveScoreReducer/liveScoreMatchReducer";
-import LiveScoreLadderState from './liveScoreReducer/liveScoreLadderReducer'
+import { combineReducers } from 'redux';
+
+import LoginState from './authenticationReducer';
+import MenuNavigationState from './menuNavigationReducer';
+import RegistrationState from './registrationReducer/registrationReducer';
+import AppState from './appReducer';
+
+/// /*****************Live Score**************************Start////
+import LiveScorePlayerState from './liveScoreReducer/liveScorePlayerReducer';
+import LiveScoreMatchState from './liveScoreReducer/liveScoreMatchReducer';
+import LiveScoreLadderState from './liveScoreReducer/liveScoreLadderReducer';
 import LiveScoreState from './liveScoreReducer/liveScoreReducer';
 import LiveScoreTeamState from './liveScoreReducer/liveScoreTeamReducer';
 import LiveScoreIncidentState from './liveScoreReducer/liveScoreIncidentReducer';
@@ -14,145 +16,140 @@ import LiveScoreNewsState from './liveScoreReducer/liveScoreNewsReducer';
 import LiveScoreBannerState from './liveScoreReducer/liveScoreBannerReducer';
 import LiveScoreRoundState from './liveScoreReducer/liveScoreRoundReducer';
 import CompetitionFeesState from './registrationReducer/competitionFeeReducer';
-import LiveScoreGoalState from './liveScoreReducer/liveScoreGoalReducer'
-import LiveScoreBulkMatchState from './liveScoreReducer/liveScoreBulkMatchReducer'
-import LiveScoreMangerState from './liveScoreReducer/liveScoreManagerReducer'
-import LiveScoreScorerState from './liveScoreReducer/liveScoreScorerReducer'
-import LiveScoreDashboardState from './liveScoreReducer/liveScoreDashboardReducer'
-import liveScoreGamePositionState from './liveScoreReducer/liveScoreGamePositionReducer'
-import liveScoreGameAttendanceState from './liveScoreReducer/liveScoreGameAttendanceReducer'
+import LiveScoreGoalState from './liveScoreReducer/liveScoreGoalReducer';
+import LiveScoreBulkMatchState from './liveScoreReducer/liveScoreBulkMatchReducer';
+import LiveScoreMangerState from './liveScoreReducer/liveScoreManagerReducer';
+import LiveScoreScorerState from './liveScoreReducer/liveScoreScorerReducer';
+import LiveScoreDashboardState from './liveScoreReducer/liveScoreDashboardReducer';
+import liveScoreGamePositionState from './liveScoreReducer/liveScoreGamePositionReducer';
+import liveScoreGameAttendanceState from './liveScoreReducer/liveScoreGameAttendanceReducer';
+/// /*****************Live Score**************************End////
 
-////*****************Live Score**************************End////
-
-import CompetitionDashboardState from './competitionManagementReducer/competitionDashboard'
-import CompetitionModuleState from './competitionManagementReducer/competitionModuleReducer'
+import CompetitionDashboardState from './competitionManagementReducer/competitionDashboard';
+import CompetitionModuleState from './competitionManagementReducer/competitionModuleReducer';
 import liveScoreCompetition from './liveScoreReducer/liveScoreCompetitionReducer';
 import CompetitionManagementState from './competitionManagementReducer/competitionManagement';
-////Year and Competition
-import CommonAppState from './appReducer'
+/// /Year and Competition
 import CompetitionFormatState from './competitionManagementReducer/competitionFormatReducer';
 import CompetitionFinalsState from './competitionManagementReducer/competitionFinalsReducer';
 import LadderFormatState from './competitionManagementReducer/ladderFormatReducer';
-import CompetitionTimeSlots from "./competitionManagementReducer/competitionTimeAndSlotsReducer";
-import CommonReducerState from "./commonReducer/commonReducer";
+import CompetitionTimeSlots from './competitionManagementReducer/competitionTimeAndSlotsReducer';
+import CommonReducerState from './commonReducer/commonReducer';
 import VenueTimeState from './competitionManagementReducer/venueTimeReducer';
-import CompetitionPartPlayerGradingState from "./competitionManagementReducer/competitionPartPlayerGradingReducer";
-import CompetitionOwnTeamGradingState from "./competitionManagementReducer/competitionTeamGradingReducer";
-import CompetitionDrawsState from "./competitionManagementReducer/competitionDrawsReducer";
-import EndUserRegistrationState from "./registrationReducer/endUserRegistrationReducer";
-import DivisionState from "./liveScoreReducer/liveScoreDivisionReducer"
-import UserState from "./userReducer/userReducer";
-import LiveScoreDivisionState from "./liveScoreReducer/liveScoreDivisionReducer"
-//liveScoreGameTimeStatisticsState
-import LiveScoreGameTimeStatisticsState from './liveScoreReducer/liveScoreGameTimeStatisticsReducer'
+import CompetitionPartPlayerGradingState from './competitionManagementReducer/competitionPartPlayerGradingReducer';
+import CompetitionOwnTeamGradingState from './competitionManagementReducer/competitionTeamGradingReducer';
+import CompetitionDrawsState from './competitionManagementReducer/competitionDrawsReducer';
+import EndUserRegistrationState from './registrationReducer/endUserRegistrationReducer';
+import UserState from './userReducer/userReducer';
+import LiveScoreDivisionState from './liveScoreReducer/liveScoreDivisionReducer';
+// liveScoreGameTimeStatisticsState
+import LiveScoreGameTimeStatisticsState from './liveScoreReducer/liveScoreGameTimeStatisticsReducer';
 import LiveScoreSetting from './liveScoreReducer/liveScoreSettingReducer';
-import LiveScoreUmpiresState from './liveScoreReducer/liveScoreUmpiresReducer'
+import LiveScoreUmpiresState from './liveScoreReducer/liveScoreUmpiresReducer';
 import LiveScoreTeamAttendanceState from './liveScoreReducer/liveScoreTeamAttendanceReducer';
 import LiveScoreVenueChangeState from './liveScoreReducer/liveScoreVenueChangeReducer';
-import RegistrationDashboardState from "./registrationReducer/registrationDashboardReducer"
-import LadderSettingState from './liveScoreReducer/liveScoreLadderSettingReducer'
-import HomeDashboardState from './homeReducer/homeReducer'
-import LiveScoreFixturCompState from './liveScoreReducer/liveScoreFixtureCompetitionReducer';
-import StripeState from "./stripeReducer/stripeReducer";
-import QuickCompetitionState from "./competitionManagementReducer/competitionQuickCompetitionReducer"
+import RegistrationDashboardState from './registrationReducer/registrationDashboardReducer';
+import LadderSettingState from './liveScoreReducer/liveScoreLadderSettingReducer';
+import HomeDashboardState from './homeReducer/homeReducer';
+import LiveScoreFixtureCompState from './liveScoreReducer/liveScoreFixtureCompetitionReducer';
+import StripeState from './stripeReducer/stripeReducer';
+import QuickCompetitionState from './competitionManagementReducer/competitionQuickCompetitionReducer';
 
-import LiveScoreCoachState from "./liveScoreReducer/liveScoreCoachReducer"
+import LiveScoreCoachState from './liveScoreReducer/liveScoreCoachReducer';
 
-import LiveScoreMatchSheetState from "./liveScoreReducer/liveScoreMatchSheetReducer"
-import ShopProductState from "./shopReducer/productReducer"
+import LiveScoreMatchSheetState from './liveScoreReducer/liveScoreMatchSheetReducer';
+import ShopProductState from './shopReducer/productReducer';
 
-//// Umpire Module
-import UmpireDashboardState from "./umpireReducer/umpireDashboardReducer"
-import UmpireCompetitionState from "./umpireReducer/umpireCompetitionReducer"
-import UmpireRoasterdState from "./umpireReducer/umpireRoasterReducer"
-import UmpireState from "./umpireReducer/umpireReducer"
-import AssignUmpireState from "./umpireReducer/assignUmpireReducer";
-import UmpireSettingState from "./umpireReducer/umpireSettingReducer"
-import RegistrationChangeState from "./registrationReducer/registrationChangeReducer";
-import ShopSettingState from "./shopReducer/shopSettingReducer";
-import InnerHorizontalState from "./liveScoreReducer/liveScoreInnerHorizontalReducer";
-import LiveScorePositionTrackState from "./liveScoreReducer/liveScorePositionTrackReducer";
-import ShopOrderSummaryState from "./shopReducer/orderSummaryReducer";
-import SupportState from "./supportReducer";
-import ShopOrderStatusState from "./shopReducer/orderStatusReducer";
+/// / Umpire Module
+import UmpireDashboardState from './umpireReducer/umpireDashboardReducer';
+import UmpireCompetitionState from './umpireReducer/umpireCompetitionReducer';
+import UmpireRoasterState from './umpireReducer/umpireRoasterReducer';
+import UmpireState from './umpireReducer/umpireReducer';
+import AssignUmpireState from './umpireReducer/assignUmpireReducer';
+import UmpireSettingState from './umpireReducer/umpireSettingReducer';
+import RegistrationChangeState from './registrationReducer/registrationChangeReducer';
+import ShopSettingState from './shopReducer/shopSettingReducer';
+import InnerHorizontalState from './liveScoreReducer/liveScoreInnerHorizontalReducer';
+import LiveScorePositionTrackState from './liveScoreReducer/liveScorePositionTrackReducer';
+import ShopOrderSummaryState from './shopReducer/orderSummaryReducer';
+import SupportState from './supportReducer';
+import ShopOrderStatusState from './shopReducer/orderStatusReducer';
 
-import UmpirePaymentSettingState from "./umpireReducer/umpirePaymentSettingReducer";
-import CompetitionMultiDrawsState from "./competitionManagementReducer/competitionMultiDrawsReducer";
-import liveScorePlayerMinuteTrackingState from "./liveScoreReducer/liveScorePlayerMinuteTrackingReducer";
+import UmpirePaymentSettingState from './umpireReducer/umpirePaymentSettingReducer';
+import CompetitionMultiDrawsState from './competitionManagementReducer/competitionMultiDrawsReducer';
+import liveScorePlayerMinuteTrackingState from './liveScoreReducer/liveScorePlayerMinuteTrackingReducer';
 
-import UmpirePaymentState from './umpireReducer/umpirePaymentReducer'
+import UmpirePaymentState from './umpireReducer/umpirePaymentReducer';
 
 const rootReducer = combineReducers({
-  LoginState,
-  MenuNavigationState,
-  RegistrationState,
-  AppState,
-  LiveScorePlayerState,
-  LiveScoreMatchState,
-  LiveScoreLadderState,
-  LiveScoreState,
-  LiveScoreTeamState,
-  LiveScoreIncidentState,
-  LiveScoreNewsState,
-  LiveScoreBannerState,
-  LiveScoreRoundState,
-  CompetitionFeesState,
-  LiveScoreGoalState,
-  LiveScoreMangerState,
-  LiveScoreScorerState,
-  LiveScoreBulkMatchState,
-  liveScoreGamePositionState,
-  CompetitionDashboardState,
-  CompetitionModuleState,
-  CommonAppState,
-  CompetitionFormatState,
-  CompetitionFinalsState,
-  LadderFormatState,
-  CompetitionTimeSlots,
-  CommonReducerState,
-  VenueTimeState,
-  CompetitionPartPlayerGradingState,
-  CompetitionOwnTeamGradingState,
-  CompetitionDrawsState,
-  LiveScoreDashboardState,
-  UserState,
-  EndUserRegistrationState,
-  DivisionState,
-  liveScoreCompetition,
-  LiveScoreDivisionState,
-  LiveScoreGameTimeStatisticsState,
-  LiveScoreSetting,
-  LiveScoreUmpiresState,
-  LiveScoreTeamAttendanceState,
-  LiveScoreVenueChangeState,
-  RegistrationDashboardState,
-  LadderSettingState,
-  HomeDashboardState,
-  LiveScoreFixturCompState,
-  StripeState,
-  QuickCompetitionState,
-  LiveScoreCoachState,
-  LiveScoreMatchSheetState,
-  CompetitionManagementState,
-  ShopProductState,
-  UmpireDashboardState,
-  UmpireCompetitionState,
-  UmpireRoasterdState,
-  UmpireState,
-  AssignUmpireState,
-  UmpireSettingState,
-  RegistrationChangeState,
-  ShopSettingState,
-  InnerHorizontalState,
-  LiveScorePositionTrackState,
-  ShopOrderSummaryState,
-  SupportState,
-  UmpirePaymentSettingState,
-  ShopOrderStatusState,
-  liveScoreGameAttendanceState,
-  CompetitionMultiDrawsState,
-  liveScorePlayerMinuteTrackingState,
-  UmpirePaymentState
+    LoginState,
+    MenuNavigationState,
+    RegistrationState,
+    AppState,
+    LiveScorePlayerState,
+    LiveScoreMatchState,
+    LiveScoreLadderState,
+    LiveScoreState,
+    LiveScoreTeamState,
+    LiveScoreIncidentState,
+    LiveScoreNewsState,
+    LiveScoreBannerState,
+    LiveScoreRoundState,
+    CompetitionFeesState,
+    LiveScoreGoalState,
+    LiveScoreMangerState,
+    LiveScoreScorerState,
+    LiveScoreBulkMatchState,
+    liveScoreGamePositionState,
+    CompetitionDashboardState,
+    CompetitionModuleState,
+    CompetitionFormatState,
+    CompetitionFinalsState,
+    LadderFormatState,
+    CompetitionTimeSlots,
+    CommonReducerState,
+    VenueTimeState,
+    CompetitionPartPlayerGradingState,
+    CompetitionOwnTeamGradingState,
+    CompetitionDrawsState,
+    LiveScoreDashboardState,
+    UserState,
+    EndUserRegistrationState,
+    liveScoreCompetition,
+    LiveScoreDivisionState,
+    LiveScoreGameTimeStatisticsState,
+    LiveScoreSetting,
+    LiveScoreUmpiresState,
+    LiveScoreTeamAttendanceState,
+    LiveScoreVenueChangeState,
+    RegistrationDashboardState,
+    LadderSettingState,
+    HomeDashboardState,
+    LiveScoreFixtureCompState,
+    StripeState,
+    QuickCompetitionState,
+    LiveScoreCoachState,
+    LiveScoreMatchSheetState,
+    CompetitionManagementState,
+    ShopProductState,
+    UmpireDashboardState,
+    UmpireCompetitionState,
+    UmpireRoasterState,
+    UmpireState,
+    AssignUmpireState,
+    UmpireSettingState,
+    RegistrationChangeState,
+    ShopSettingState,
+    InnerHorizontalState,
+    LiveScorePositionTrackState,
+    ShopOrderSummaryState,
+    SupportState,
+    UmpirePaymentSettingState,
+    ShopOrderStatusState,
+    liveScoreGameAttendanceState,
+    CompetitionMultiDrawsState,
+    liveScorePlayerMinuteTrackingState,
+    UmpirePaymentState,
 });
 
 export default rootReducer;
