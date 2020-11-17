0e7197d1c54a4ca3838418f50ae9b2611ccd4469
nininv <nininv45@gmail.com>
2020-11-17 14:14:51 +0100

    Fix issue on login TFA

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -1,39 +1,39 @@
-import ApiConstants from "../../themes/apiConstants";
+import ApiConstants from "themes/apiConstants";
 
 function loginAction(payload) {
-  return {
-    type: ApiConstants.API_LOGIN_LOAD,
-    payload
-  };
+    return {
+        type: ApiConstants.API_LOGIN_LOAD,
+        payload,
+    };
 }
 
 function qrSubmitAction(payload) {
-  return {
-    type: ApiConstants.API_QR_CODE_LOAD,
-    payload
-  };
+    return {
+        type: ApiConstants.API_QR_CODE_LOAD,
+        payload,
+    };
 }
 
 // forgot password
 function forgotPasswordAction(email, resetType) {
-  return {
-    type: ApiConstants.API_FORGOT_PASSWORD_LOAD,
-    email,
-    resetType,
-  };
+    return {
+        type: ApiConstants.API_FORGOT_PASSWORD_LOAD,
+        email,
+        resetType,
+    };
 }
 
 // clear reducer
 function clearReducerAction(key) {
-  return {
-    type: ApiConstants.ACTION_TO_CLEAR_AUTHENTICATION_REDUCER,
-    key
-  };
+    return {
+        type: ApiConstants.ACTION_TO_CLEAR_AUTHENTICATION_REDUCER,
+        key,
+    };
 }
 
 export {
-  loginAction,
-  qrSubmitAction,
-  forgotPasswordAction,
-  clearReducerAction,
-}
+    loginAction,
+    qrSubmitAction,
+    forgotPasswordAction,
+    clearReducerAction,
+};
