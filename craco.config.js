c093510944c74011d3f65f9acf0ccac419b80ec2
nininv <nininv45@gmail.com>
2021-01-05 23:57:00 +0100

    Update theme config using less

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,30 @@
-const CracoAntDesignPlugin = require("craco-antd");
+const CracoLessPlugin = require('craco-less');
 
 module.exports = {
     plugins: [
         {
-            plugin: CracoAntDesignPlugin,
+            plugin: CracoLessPlugin,
             options: {
-                customizeTheme: {
-                    "@primary-color": "#ff8237",
-                    "@primary-1": "#ff8237",
-                    "@primary-2": "#ff8237",
-                    "@link-color": "#ff8237",
-                    "@link-hover-color": "#ff8237",
-                    "@link-active-color": "#ff8237",
-                    "@height-base": "40px",
-                    "@height-lg": "48px",
-                    "@height-sm": "32px",
-                    "@font-size-base": "15px",
-                    "@btn-font-weight": 600,
-                    "@btn-border-radius-base": "8px",
-                }
-            }
-        }
-    ]
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {
+                            '@primary-color': '#ff8237',
+                            '@primary-1': '#ff8237',
+                            '@primary-2': '#ff8237',
+                            '@link-color': '#ff8237',
+                            '@link-hover-color': '#ff8237',
+                            '@link-active-color': '#ff8237',
+                            '@height-base': '40px',
+                            '@height-lg': '48px',
+                            '@height-sm': '32px',
+                            '@font-size-base': '15px',
+                            '@btn-font-weight': 600,
+                            '@btn-border-radius-base': '8px',
+                        },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
 };
