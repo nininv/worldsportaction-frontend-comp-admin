6064d207779e31115bdc73817e4d9af6fdbae985
nininv <nininv45@gmail.com>
2021-01-07 18:00:12 +0100

    Revert craco less configuration

--- craco.config.js
+++ craco.config.js
@@ -1,28 +1,23 @@
-const CracoLessPlugin = require('craco-less');
+const CracoAntDesignPlugin = require('craco-antd');
 
 module.exports = {
     plugins: [
         {
-            plugin: CracoLessPlugin,
+            plugin: CracoAntDesignPlugin,
             options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: {
-                            '@primary-color': '#ff8237',
-                            '@primary-1': '#ff8237',
-                            '@primary-2': '#ff8237',
-                            '@link-color': '#ff8237',
-                            '@link-hover-color': '#ff8237',
-                            '@link-active-color': '#ff8237',
-                            '@height-base': '40px',
-                            '@height-lg': '48px',
-                            '@height-sm': '32px',
-                            '@font-size-base': '15px',
-                            '@btn-font-weight': 600,
-                            '@btn-border-radius-base': '8px',
-                        },
-                        javascriptEnabled: true,
-                    },
+                customizeTheme: {
+                    '@primary-color': '#ff8237',
+                    '@primary-1': '#ff8237',
+                    '@primary-2': '#ff8237',
+                    '@link-color': '#ff8237',
+                    '@link-hover-color': '#ff8237',
+                    '@link-active-color': '#ff8237',
+                    '@height-base': '40px',
+                    '@height-lg': '48px',
+                    '@height-sm': '32px',
+                    '@font-size-base': '15px',
+                    '@btn-font-weight': 600,
+                    '@btn-border-radius-base': '8px',
                 },
             },
         },
