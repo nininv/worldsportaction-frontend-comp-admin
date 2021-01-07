07a922652705fd9356d67e8fd8c08d2bf16f0a3c
nininv <nininv45@gmail.com>
2021-01-07 19:26:58 +0100

    Revert "Update theme config using less"
    This reverts commit b1d00bea

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './style.less';
+import './style.css';
 
 import App from './App';
 import ConfigureStore from './configureStore';
