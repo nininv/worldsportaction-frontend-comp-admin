83ab5cfa647145eea220bf5bf37365f6cc1ee28f
nininv <nininv45@gmail.com>
2021-01-12 15:52:26 +0530

    CM - 1813

--- src/components/user/userModulePersonalDetail.jsx
+++ src/components/user/userModulePersonalDetail.jsx
@@ -377,8 +377,8 @@ const teamMembersColumns = [
     },
     {
         title: "Status",
-        dataIndex: "name",
-        key: "name",
+        dataIndex: "paymentStatus",
+        key: "paymentStatus",
     },
     {
         title: "Paid Fee",
