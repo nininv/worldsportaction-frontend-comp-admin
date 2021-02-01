8d8155e6c43126c095111a283459d71bff5e35ec
nininv <nininv45@gmail.com>
2021-02-01 19:11:47 +0200

    refactor: remove unnecessary code

--- src/components/umpire/umpireDivisions.jsx
+++ src/components/umpire/umpireDivisions.jsx
@@ -18,11 +18,6 @@ import { getRefBadgeData } from '../../store/actions/appAction'
 const { Header, Footer, } = Layout
 const { Option } = Select
 
-const allocatePools = [
-    { id: 1, name: "Division" },
-    { id: 2, name: "Grades" }
-]
-
 class UmpireDivisions extends Component {
     constructor(props) {
         super(props)
@@ -144,18 +139,6 @@ class UmpireDivisions extends Component {
         let badge = isArrayNotEmpty(badgeData) ? badgeData : []
         return (
             <div className="content-view pt-5">
-                <span className="text-heading-large">{AppConstants.allocatePools}</span>
-                <Radio.Group
-                    className="reg-competition-radio"
-                    // onChange={e => this.props.add_editcompetitionFeeDeatils(e.target.value, "competitionTypeRefId")}
-                    // onChange={e => this.setPools(e.target.value)}
-                    // value={detailsData.competitionTypeRefId}
-                    // disabled={compDetailDisable}
-                >
-                    {allocatePools.map((item) => (
-                        <Radio key={'allocatePool_' + item.id} value={item.id}>{item.name}</Radio>
-                    ))}
-                </Radio.Group>
 
                 <span className='text-heading-large pt-3 mb-0' >{AppConstants.umpirePools}</span>
 
@@ -185,191 +168,19 @@ class UmpireDivisions extends Component {
         )
     }
 
-    contentView_1 = () => {
-        // const { badgeData } = this.props.appState
-
-        return (
-            <div className="content-view pt-5">
-                <span className="text-heading-large">{AppConstants.allocatePools}</span>
-                <Radio.Group
-                    className="reg-competition-radio"
-                    // onChange={e => this.props.add_editcompetitionFeeDeatils(e.target.value, "competitionTypeRefId")}
-                    // onChange={e => this.setPools(e.target.value)}
-                    // value={detailsData.competitionTypeRefId}
-                    // disabled={compDetailDisable}
-                >
-                    {allocatePools.map((item) => (
-                        <Radio key={'allocatePool_' + item.id} value={item.id}>{item.name}</Radio>
-                    ))}
-                </Radio.Group>
-
-                <span className='text-heading-large pt-3 mb-0' >{AppConstants.umpirePools}</span>
-                <div className="row pt-3">
-                    <div className='col-sm-3 division-table-field-view'>
-                        <InputWithHead heading={AppConstants.badgeAA} />
-                    </div>
-                    <div className="col-sm">
-                        <Select
-                            placeholder="Select"
-                            mode="multiple"
-                            style={{ width: "100%", paddingRight: 1, minWidth: 182 }}
-                        >
-                            <Option value="a">A Grade</Option>
-                            <Option value="b">B Grade</Option>
-                            <Option value="c">C Grade</Option>
-                        </Select>
-                    </div>
-                </div>
-                <div className="row pt-3">
-                    <div className='col-sm-3 division-table-field-view'>
-                        <InputWithHead heading={AppConstants.badgeA} />
-                    </div>
-                    <div className="col-sm">
-                        <Select
-                            placeholder="Select"
-                            mode="multiple"
-                            style={{ width: "100%", paddingRight: 1, minWidth: 182 }}
-                            // onChange={umpirePool => this.onChangeUmpirePools({ key: "recordUmpire", data: umpirePool })}
-                            // value={this.state.umpPool}
-                        >
-                            <Option value="aGrade">A Grade</Option>
-                            <Option value="bGrade">B Grade</Option>
-                            <Option value="cGrade">C Grade</Option>
-                        </Select>
-                    </div>
-                </div>
-
-                <div className="row pt-3">
-                    <div className='col-sm-3 division-table-field-view'>
-                        <InputWithHead heading={AppConstants.badgeB} />
-                    </div>
-                    <div className="col-sm">
-                        <Select
-                            placeholder="Select"
-                            mode="multiple"
-                            style={{ width: "100%", paddingRight: 1, minWidth: 182 }}
-                            // onChange={recordUmpire => this.props.onChangeUmpirePools({ key: "recordUmpire", data: recordUmpire })}
-                            // value={this.state.umpPool}
-                        >
-                            <Option value="aGradea">A Grade</Option>
-                            <Option value="bGradeb">B Grade</Option>
-                            <Option value="cGradec">C Grade</Option>
-                        </Select>
-                    </div>
-                </div>
-
-                <div className="row pt-3">
-                    <div className='col-sm-3 division-table-field-view'>
-                        <InputWithHead heading={AppConstants.badgeC} />
-                    </div>
-                    <div className="col-sm">
-                        <Select
-                            placeholder="Select"
-                            mode="multiple"
-                            style={{ width: "100%", paddingRight: 1, minWidth: 182 }}
-                            // onChange={recordUmpire => this.props.onChangeUmpirePools({ key: "recordUmpire", data: recordUmpire })}
-                            // value={this.state.umpPool}
-                        >
-                            <Option value="aGradeaa">A Grade</Option>
-                            <Option value="bGradebb">B Grade</Option>
-                            <Option value="cGradecc">C Grade</Option>
-                        </Select>
-                    </div>
-                </div>
-
-                <div className="row pt-3">
-                    <div className='col-sm-3 division-table-field-view'>
-                        <InputWithHead heading={AppConstants.umpireCoach} />
-                    </div>
-                    <div className="col-sm">
-                        <Select
-                            placeholder="Select"
-                            mode="multiple"
-                            style={{ width: "100%", paddingRight: 1, minWidth: 182 }}
-                            // onChange={recordUmpire => this.props.onChangeUmpirePools({ key: "recordUmpire", data: recordUmpire })}
-                            // value={this.state.umpPool}
-                        >
-                            <Option value="Gradeaa">A Grade</Option>
-                            <Option value="Gradebb">B Grade</Option>
-                            <Option value="Gradecc">C Grade</Option>
-                        </Select>
-                    </div>
-                </div>
-
-                {/* <span className="text-heading-large pt-5">{AppConstants.simultaneousMatchAllocations}</span> */}
-                {/* <div className="row pt-3">
-                    <div className="col-sm-2">
-                        <InputWithHead heading={AppConstants.poolName} />
-                        <InputWithHead heading="Badge AA" />
-                    </div>
-                    <div className="col-sm-3">
-                        <InputWithHead
-                            auto_complete="new-umpireReserve"
-                            heading={AppConstants.umpireReserve}
-                            placeholder="Umpire Reserve"
-                            onChange={(e) => this.setState({ umpireReserve: e.target.value })}
-                            value={this.state.umpireReserve}
-                        />
-                    </div>
-                    <div className="col-sm-3">
-                        <InputWithHead
-                            auto_complete="new-umpireCoach"
-                            heading={AppConstants.umpireCoach}
-                            placeholder="Umpire Coach"
-                            onChange={(e) => this.setState({ umpireCoach: e.target.value })}
-                            value={this.state.umpireCoach}
-                        />
-                    </div>
-                </div> */}
-
-                {/* <div className="row pt-3">
-                    <div className='col-sm-2'>
-                        <InputWithHead heading="Badge A" />
-                    </div>
-                    <div className='col-sm-3'>
-                        <InputWithHead
-                            auto_complete='new-umpireReserve'
-                            placeholder="Umpire Reserve"
-                            onChange={(e) => this.setState({ umpireReserve2: e.target.value })}
-                            value={this.state.umpireReserve2}
-                        />
-                    </div>
-                    <div className='col-sm-3'>
-                        <InputWithHead
-                            auto_complete='new-umpireCoach'
-                            placeholder="Umpire Coach"
-                            onChange={(e) => this.setState({ umpireCoach2: e.target.value })}
-                            value={this.state.umpireCoach2}
-                        />
-                    </div>
-                </div> */}
-            </div>
-        )
-    }
-
     //////footer view containing all the buttons like submit and cancel
     footerView = () => {
         return (
             <div className="fluid-width">
                 <div className="footer-view">
-                    <div className="row">
-                        <div className="col-sm">
-                            <div className="reg-add-save-button">
-                                <NavLink to='/umpirePoolAllocation'>
-                                    <Button className="cancelBtnWidth" type="cancel-button">{AppConstants.back}</Button>
-                                </NavLink>
-                            </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="comp-buttons-view">
-                                <Button className="publish-button save-draft-text" type="primary" htmlType="submit">
-                                    {/* {AppConstants.generateRoster} */}
-                                    {AppConstants.save}
-                                </Button>
-                                <Button onClick={() => history.push("/umpireDashboard")} className="open-reg-button" type="primary" htmlType="submit">
-                                    {AppConstants.createRoster}
-                                </Button>
-                            </div>
+                    <div className="col-sm">
+                        <div className="comp-buttons-view">
+                            <Button className="publish-button save-draft-text" type="primary" htmlType="submit">
+                                {AppConstants.save}
+                            </Button>
+                            <Button onClick={() => history.push("/umpireDashboard")} className="open-reg-button" type="primary" htmlType="submit">
+                                {AppConstants.createRoster}
+                            </Button>
                         </div>
                     </div>
                 </div>
