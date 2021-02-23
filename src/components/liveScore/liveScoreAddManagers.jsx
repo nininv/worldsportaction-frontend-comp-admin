2202d2d44a9c59b55ed1ff1971c16e2ccba9850a
nininv <nininv45@gmail.com>
2021-02-23 14:47:39 +0200

    bugfix: correct compId variable

--- src/components/liveScore/liveScoreAddManagers.jsx
+++ src/components/liveScore/liveScoreAddManagers.jsx
@@ -54,10 +54,10 @@ class LiveScoreAddManager extends Component {
 
     componentDidMount() {
         if (getLiveScoreCompetiton()) {
-            const { id, competitionOrganisation, competitionOrganisationId } = JSON.parse(getLiveScoreCompetiton())
-            let compOrgId = competitionOrganisation ? competitionOrganisation.id : competitionOrganisationId ? competitionOrganisationId : 0
             checkLivScoreCompIsParent().then((value) => {
-                this.props.liveScoreManagerListAction(5, value ? 1 : 6, null, null, null, null, null, null, null, compOrgId)
+                const { id, competitionOrganisation, competitionOrganisationId } = JSON.parse(getLiveScoreCompetiton())
+                let compOrgId = competitionOrganisation ? competitionOrganisation.id : competitionOrganisationId ? competitionOrganisationId : 0
+                this.props.liveScoreManagerListAction(5, value ? 1 : 6, null, null, null, null, null, null, null, value ? id : compOrgId)
                 this.props.getliveScoreTeams(id, null, compOrgId)
                 if (this.state.isEdit === true) {
                     this.props.liveScoreUpdateManagerDataAction(this.state.tableRecord, 'isEditManager')
