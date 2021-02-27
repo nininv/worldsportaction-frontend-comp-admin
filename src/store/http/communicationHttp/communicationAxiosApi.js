aba0969c8e54ba21e27693fa0d8ef0c1e341d8ea
nininv <nininv45@gmail.com>
2021-02-27 19:11:10 +1100

    comms year ref

--- src/store/http/communicationHttp/communicationAxiosApi.js
+++ src/store/http/communicationHttp/communicationAxiosApi.js
@@ -19,7 +19,7 @@ const communicationAxiosApi = {
             form.append('toOrganisationIds', data.toOrganisationIds);
             form.append('toUserRoleIds', data.toUserRoleIds);
             form.append('toUserIds', data.toUserIds);
-            form.append('yearId', data.yearId);
+            form.append('yearRefId', data.yearRefId);
 
             if (data.imageUrl) {
                 form.append('imageUrl', data.imageUrl);
