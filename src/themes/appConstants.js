2a393c14d03ee57898717970f31804008368dad6
nininv <nininv45@gmail.com>
2021-03-23 19:21:59 +0700

    CM-2756 Add payment method column in Summary participant

--- src/themes/appConstants.js
+++ src/themes/appConstants.js
@@ -2037,6 +2037,7 @@ const AppConstants = {
     governmentVoucherNumber: "Government Voucher Number",
     incorrectProductDetails: "You have entered incorrect product details.",
     voucherOrDiscountCode: "Voucher/Discount Code",
+    cash: "Cash",
 };
 
 export default AppConstants;
