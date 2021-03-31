07dd826d3c2195d088d13116b8249f9baf15cb99
nininv <nininv45@gmail.com>
2021-04-01 03:04:53 +1100

    cm-1731 hide admin terms for non state

--- src/components/user/userOurOrganization.jsx
+++ src/components/user/userOurOrganization.jsx
@@ -968,8 +968,7 @@ class UserOurOrganization extends Component {
         const affiliate = this.props.userState.affiliateOurOrg;
         return (
             <>
-                {((getOrganisationData() && getOrganisationData().organisationTypeRefId == 2 && this.state.sourcePage != "DIR")
-                                        || (this.state.organisationTypeRefId == 2 && this.state.sourcePage == "DIR")) && <div className="discount-view pt-5">
+                 <div className="discount-view pt-5">
                     <span className="form-heading">{AppConstants.termsAndConditions}</span>
                     <Radio.Group
                         className="reg-competition-radio"
@@ -1018,7 +1017,10 @@ class UserOurOrganization extends Component {
                             </div>
                         )}
                     </Radio.Group>
-                </div>}
+                </div>
+            
+                {((getOrganisationData() && getOrganisationData().organisationTypeRefId == 2 && this.state.sourcePage != "DIR")
+                                        || (this.state.organisationTypeRefId == 2 && this.state.sourcePage == "DIR")) && 
                 <div className="discount-view pt-5">
                     <span className="form-heading">{AppConstants.stateTermsAndConditions}</span>
                     <Radio.Group
@@ -1069,7 +1071,7 @@ class UserOurOrganization extends Component {
                             </div>
                         )}
                     </Radio.Group>
-                </div>
+                </div>}
             </>
         )
     }
