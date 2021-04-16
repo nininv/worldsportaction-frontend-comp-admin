423b3b683b81c2263c704ccd657a965929c08c00
nininv <nininv45@gmail.com>
2021-04-16 15:09:17 +0300

    Update payments reducer

--- src/store/reducer/umpireReducer/umpirePaymentReducer.js
+++ src/store/reducer/umpireReducer/umpirePaymentReducer.js
@@ -19,49 +19,20 @@ const initialState = {
 };
 
 function getFilterUmpirePayment(umpirePaymentArr) {
-    let arr = []
-    for (let i in umpirePaymentArr) {
-
-        if (umpirePaymentArr[i].umpireType === "USERS") {
-            let obj = {
-                approvedByUser: umpirePaymentArr[i].approvedByUser,
-                approved_at: umpirePaymentArr[i].approved_at,
-                approvedByUserId: umpirePaymentArr[i].approvedByUserId,
-                id: umpirePaymentArr[i].id,
-                match: umpirePaymentArr[i].match,
-                matchId: umpirePaymentArr[i].matchId,
-                organisationId: umpirePaymentArr[i].organisationId,
-                paymentStatus: umpirePaymentArr[i].paymentStatus,
-                sequence: umpirePaymentArr[i].sequence,
-                umpireName: umpirePaymentArr[i].umpireName,
-                umpireType: umpirePaymentArr[i].umpireType,
-                user: umpirePaymentArr[i].user,
-                userId: umpirePaymentArr[i].userId,
-                verifiedBy: umpirePaymentArr[i].verifiedBy,
-                selectedValue: umpirePaymentArr[i].paymentStatus === "paid" || umpirePaymentArr[i].paymentStatus === "approved" ? true : false,
-                hoverVisible: false
-            }
-            arr.push(obj)
-        }
-    }
-    return arr;
+    return umpirePaymentArr.map(payment => ({
+        ...payment, 
+        selectedValue: payment.paymentStatus === "paid" || payment.paymentStatus === "approved"
+    }));
 }
 
-function getPayementTransferFilterData(umpirePaymentArr) {
-    let arr = []
-    for (let i in umpirePaymentArr) {
-
-        if (umpirePaymentArr[i].paymentStatus === "approved") {
-
-            let obj = {
-                userId: umpirePaymentArr[i].user.id,
-                matchUmpireId: umpirePaymentArr[i].id,
-                stripeId: umpirePaymentArr[i].user.stripeAccountId
-            }
-            arr.push(obj)
-        }
-    }
-    return arr;
+function getPaymentTransferFilterData(umpirePaymentArr) {
+    return umpirePaymentArr
+        .filter(umpirePayment => umpirePayment.paymentStatus === "approved")
+        .map(umpirePayment => ({ 
+            userId:  umpirePayment.userId,
+            matchUmpireId: umpirePayment.id,
+            stripeId: umpirePayment.user.stripeAccountId
+        }));
 }
 
 function getAllSelectedBoxData(umpirePaymentArr) {
@@ -77,134 +48,98 @@ function umpirePaymentState(state = initialState, action) {
         case ApiConstants.API_GET_UMPIRE_PAYMENT_DATA_SUCCESS:
             let result = action.result.players
             let filterUmpirePayment = getFilterUmpirePayment(result)
-            let payementTransferFilterData = getPayementTransferFilterData(result)
+            let paymentTransferFilterData = getPaymentTransferFilterData(result)
             let filterDataForAllSelectedBox = getAllSelectedBoxData(result)
-            state.allSelectedData = filterDataForAllSelectedBox
-
-            if ((state.allSelectedData.length > 0 && result.length > 0) && (state.allSelectedData.length === result.length)) {
-                state.paymentStatus = true
-            } else {
-                state.paymentStatus = false
-            }
-            state.paymentTransferPostData = JSON.parse(JSON.stringify(payementTransferFilterData))
-            state.paymentArr = JSON.parse(JSON.stringify(payementTransferFilterData))
-            let pages = action.result.page
 
             return {
                 ...state,
                 onLoad: false,
                 umpirePaymentList: filterUmpirePayment,
-                totalCount: pages.totalCount,
-                currentPage: pages.currentPage,
-                status: action.status
+                paymentTransferPostData: paymentTransferFilterData,
+                paymentArr: paymentTransferFilterData,
+                totalCount: action.result?.page?.totalCount,
+                currentPage: action.result?.page?.currentPage,
+                status: action.status,
+                allSelectedData: filterDataForAllSelectedBox,
+                paymentStatus: filterDataForAllSelectedBox.length && result.length && (filterDataForAllSelectedBox.length === result.length)
             };
 
         case ApiConstants.API_UPDATE_UMPIRE_PAYMENT_DATA:
             const { data, key, index = null, allData = {} } = action.data;
-            let umpirePaymentArr = state.umpirePaymentList;
-            let postData = state.paymentTransferPostData;
-            if (key === 'allCheckBox') {
-                for (let i in umpirePaymentArr) {
-                    if (umpirePaymentArr[i].paymentStatus === "unpaid") {
-                        umpirePaymentArr[i]["selectedValue"] = data
+            let umpirePaymentArr = [...state.umpirePaymentList];
+            let postData = [...state.paymentTransferPostData];
+            switch (key) {
+                case 'allCheckBox':
+                    const selectedPayments = umpirePaymentArr.map(
+                        payment => payment.paymentStatus === 'unpaid' 
+                            ? ({ ...payment, selectedValue: data })
+                            : payment
+                    )
+                    const unpaidPayments = selectedPayments.filter(
+                        payment => payment.paymentStatus === 'unpaid'
+                    );
+                    const payData = data ? unpaidPayments : [];
+    
+                    const newPaymentTransfer = !!payData.length 
+                        ? payData.filter(payment => payment.user && payment.user.stripeAccountId)
+                            .map(payment => ({
+                                userId: payment.userId || payment.user?.id,
+                                matchUmpireId: payment?.id,
+                                stripeId: payment.user.stripeAccountId
+                            }))
+                        : [...state.paymentArr];
+    
+                    return {
+                       ...state,
+                       umpireDataPayement: payData,
+                       paymentTransferPostData: newPaymentTransfer,
+                       umpirePaymentList: selectedPayments,
+                       paymentStatus: data,
                     }
 
-                    if (data) {
-                        if (umpirePaymentArr[i].paymentStatus === "unpaid") {
-                            state.umpireDataPayement.push(umpirePaymentArr[i])
-                        }
+                case 'selectedValue':
+                    let userId = allData.userId
+                    let matchUmpireId = allData.id
+                    let stripeId = allData.user ? allData.user.stripeAccountId : null
+
+                    const targetIndex = postData.findIndex(record => record.matchUmpireId === matchUmpireId)
+                    // Update paymentTransferPostData
+                    if (data) { 
+                        postData.push({ userId, matchUmpireId, stripeId});
                     } else {
-                        state.umpireDataPayement = []
-                    }
-
-
-                    // if (data && umpirePaymentArr[i].paymentStatus === 'unpaid' && umpirePaymentArr[i].user && umpirePaymentArr[i].user.stripeAccountId) {
-                    //     let obj = {
-                    //         userId: umpirePaymentArr[i].user.id,
-                    //         matchUmpireId: umpirePaymentArr[i].id,
-                    //         stripeId: umpirePaymentArr[i].user.stripeAccountId
-                    //     }
-
-                    //     state.paymentTransferPostData.push(obj)
-                    // } else {
-
-                    //     if (umpirePaymentArr[i].paymentStatus === 'unpaid') {
-                    //         state.paymentTransferPostData.splice(i, 1)
-                    //     }
-                    // }
-
-                }
-                let payData = state.umpireDataPayement
-                if (payData.length > 0) {
-                    for (let i in payData) {
-
-                        if (payData[i].user && payData[i].user.stripeAccountId) {
-                            let obj = {
-                                userId: payData[i].user.id,
-                                matchUmpireId: payData[i].id,
-                                stripeId: payData[i].user.stripeAccountId
-                            }
-                            state.paymentTransferPostData.push(obj)
+                        if (targetIndex > -1) {
+                            postData.splice(targetIndex, 1)
                         }
                     }
-                } else {
-                    state.paymentTransferPostData = state.paymentArr
-                }
 
+                    // Update umpirePaymentList
+                    const umpireIndex = umpirePaymentArr.findIndex(record => record.id === matchUmpireId);
+                    if (umpireIndex > -1) umpirePaymentArr[umpireIndex].selectedValue = data;
+                    const newPaymentStatus = umpirePaymentArr.every(payment => !!payment.selectedValue);
 
-                state.paymentStatus = data
-                state.umpirePaymentList = umpirePaymentArr
-
-
-            } else if (key === 'selectedValue') {
-                let userId = allData.userId
-                let matchUmpireId = allData.id
-                let stripeId = allData.user ? allData.user.stripeAccountId : null
-
-                const targetIndex = postData.findIndex(record => record.matchUmpireId === matchUmpireId)
-                // Update paymentTransferPostData
-                if (data) { 
-                    postData.push({ userId, matchUmpireId, stripeId});
-                } else {
-                    if (targetIndex > -1) {
-                        postData.splice(targetIndex, 1)
-                    }
-                }
-
-                // Update umpirePaymentList
-                const umpireIndex = umpirePaymentArr.findIndex(record => record.id === matchUmpireId);
-                if (umpireIndex > -1) umpirePaymentArr[umpireIndex].selectedValue = data;
-                const newPaymentStatus = umpirePaymentArr.every(payment => !!payment.selectedValue);
-                // let updatedPayment = umpireIndex > -1 ? umpirePaymentArr[umpireIndex] : null
-                // if (!data && updatedPayment && updatedPayment.paymentStatus === 'approved') { 
-                //     updatedPayment.approvedByUser = null;
-                //     updatedPayment.approved_at = null;
-                //     updatedPayment.approvedByUserId = null;
-                //     updatedPayment.paymentStatus = 'unpaid';
-                //     updatedPayment.selectedValue = false;
-                //     umpirePaymentArr[umpireIndex] = updatedPayment;
-                // }
-
-                return {
-                    ...state,
-                    umpirePaymentList: umpirePaymentArr,
-                    paymentTransferPostData: postData,
-                    paymentStatus: newPaymentStatus
-                }
-
-            } else if (key === "clearData") {
-                state.paymentTransferPostData = []
-                state.paymentStatus = null
-                for (let i in state.umpirePaymentList) {
-                    if (state.umpirePaymentList[i].paymentStatus === "unpaid") {
-                        state.umpirePaymentList[i]['selectedValue'] = false
+                    return {
+                        ...state,
+                        umpirePaymentList: umpirePaymentArr,
+                        paymentTransferPostData: postData,
+                        paymentStatus: newPaymentStatus
                     }
 
-                }
+                case 'clearData':
+                    const clearedPayments = umpirePaymentArr.map(
+                        payment => payment.paymentStatus === 'unpaid' 
+                            ? ({ ...payment, selectedValue: false })
+                            : payment
+                    )
+
+                    return {
+                        ...state,
+                        paymentTransferPostData: [],
+                        paymentStatus: false,
+                        umpirePaymentList: clearedPayments
+                    }
+
+                default: return {...state} ;
             }
-            return {
-                ...state,
-            };
 
         case ApiConstants.API_UMPIRE_FAIL:
             return {
@@ -227,37 +162,23 @@ function umpirePaymentState(state = initialState, action) {
             return { ...state, onPaymentLoad: true };
 
         case ApiConstants.API_UMPIRE_PAYMENT_TRANSFER_DATA_SUCCESS:
-            return {
-                ...state,
-                onPaymentLoad: false,
-            };
+            return { ...state, onPaymentLoad: false };
 
         case ApiConstants.ONCHANGE_COMPETITION_CLEAR_DATA_FROM_LIVESCORE:
             state.umpirePaymentObject = null
             return { ...state, onLoad: false };
 
         case ApiConstants.API_UMPIRE_PAYMENT_EXPORT_FILE_LOAD:
-
-
             return { ...state, onPaymentLoad: true };
 
         case ApiConstants.API_UMPIRE_PAYMENT_EXPORT_FILE_SUCCESS:
-            return {
-                ...state,
-                onPaymentLoad: false,
-            };
+            return { ...state, onPaymentLoad: false };
 
         case ApiConstants.SET_UMPIRE_PAYMENT_LIST_PAGE_SIZE:
-            return {
-                ...state,
-                pageSize: action.pageSize,
-            }
+            return { ...state, pageSize: action.pageSize || 10 }
         
         case ApiConstants.SET_UMPIRE_ROSTER_LIST_PAGE_CURRENT_NUMBER:
-            return {
-                ...state,
-                currentPage: action.pageNum,
-            }
+            return { ...state, currentPage: action.pageNum }
 
         default:
             return state;
